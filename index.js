130ea6273bd1877a748057653aa0900062fea557
Ayush <ayushsaxena917@gmail.com>
2024-06-19 21:25:19 +0530

    Project for freecodecamp certification

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+let urlArr = [];
+
+app.post("/api/shorturl", (req, res) => {
+  const url = req.body.url;
+  let found = urlArr.find((el) => el.original_url === url);
+  if (found) {
+    return res.json(found);
+  }
+  if (!url.startsWith("https://") && !url.startsWith("http://")) {
+    return res.json({
+      error: "invalid url",
+    });
+  }
+  const { hostname } = new URL(url);
+  dns.lookup(hostname, (err, address) => {
+    if (err) {
+      return res.json({
+        error: "invalid url",
+      });
+    }
+    const newObj = { original_url: url, short_url: urlArr.length + 1 };
+    urlArr.push(newObj);
+    return res.json(newObj);
+  });
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const found = urlArr.find((el) => el.short_url === id);
+  if (!found) {
+    return res.json({ error: "No short URL found for the given input" });
+  }
+  res.redirect(found.original_url);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });

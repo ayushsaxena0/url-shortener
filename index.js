a675877d0370669206889d6de74530e6bec8aa01
Ayush <ayushsaxena917@gmail.com>
2024-06-26 22:02:17 +0530

    Changing dirname

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // Your first API endpoint
